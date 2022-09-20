d97182d9d8292d14dd614e545d99312589cafb55
李瑞丰 <liruifeng1024@gmail.com>
2022-09-20 19:45:15 +0800

    feat: 从命令行读取 markdown 文件 (#1)
    Co-authored-by: 李瑞丰🤡 <lirf@lenovocloud.com>

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# rust-template
+# md-parser
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-  println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+
+fn main() -> std::io::Result<()> {
+  println!("Hello, 这里是一个 Markdown 解析器!");
+
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() > 1 {
+    let target = &args[1];
+
+    if Path::new(target).is_file() {
+      let mut file = fs::File::open(target).unwrap();
+
+      let mut contents = String::new();
+      file.read_to_string(&mut contents).unwrap();
+      println!("读取到的 Markdown 内容是 {}", contents);
+
+      Ok(())
+    } else {
+      panic!("请选择 Markdown 文件")
+    }
+  } else {
+    panic!("请选择 Markdown 文件")
+  }
 }
